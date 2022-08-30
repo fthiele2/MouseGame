9d676948cae35b972d7bfb0c914e8a8601e9e200
fthiele2 <fthiele2@hs-mittweida.de>
2022-08-30 10:51:13 +0200

    GameOver/Win

--- Assets/Timer/CountdownTimer.cs
+++ Assets/Timer/CountdownTimer.cs
@@ -6,9 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class CountdownTimer : MonoBehaviour
 {
-    public string SceneToLoad;
-    private float currentTime = 60f;
+    private float currentTime = 5f;
     private Text countdownText;
+    public Transform player;
+
+    public GameObject GameOverScreen;
+    public GameObject GameWinScreen;
+    public GameObject Background; 
 
 
     // Use this for initialization
@@ -24,8 +28,24 @@ public class CountdownTimer : MonoBehaviour
         countdownText.text = currentTime.ToString("f0");
         if (currentTime <= 0)
         {
-            SceneManager.LoadScene(SceneToLoad);
+            GameOver();
+            Background.SetActive(true);
         }
 
+    }
+
+    void GameOver()
+    {
+        if(currentTime <= 0 && player.gameObject.tag == "Infected")
+        {
+            Debug.Log("GameOver");
+            GameOverScreen.SetActive(true);
+        }
+
+        else
+        {
+            Debug.Log("Win");
+            GameOverScreen.SetActive(true);
+        }
     }
 }
